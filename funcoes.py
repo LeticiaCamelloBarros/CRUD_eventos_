9b9c5d08f7509ba236bc08480f1ecdd162bc928a
brncluis <fockluis@gmail.com>
2025-11-08 22:33:41 -0300

    Inseri uma nova funcionalidade onde a organizadora de eventos consegue calcular o lucro bruto sobre cada evento

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,6 @@
+import os
+from datetime import datetime
+
 def adicionar(nome_evento, tipo_evento, data_evento, local_evento, orcamento):
     dados = [nome_evento, tipo_evento, data_evento, local_evento, orcamento]
     nome_evento_arquivo = nome_evento.replace(' ', '_')
@@ -12,3 +15,141 @@ def visualizar(nome_evento):
     with open(arquivo_nome, "r", encoding="utf-8") as arquivo:
         for linha in arquivo:
             print(linha.strip())
+
+def excluir(nome_evento):
+    nome_evento_arquivo = nome_evento.replace(' ', '_')
+    arquivo_nome = f"{nome_evento_arquivo}.txt"
+    os.remove(arquivo_nome)
+
+def editar(nome_evento):
+    nome_evento_arquivo = nome_evento.replace(' ', '_')
+    arquivo_nome = f"{nome_evento_arquivo}.txt"
+    dados_novos = []
+
+    opcao = input("Qual informação que você deseja alterar: \nNome do evento \nTipo do evento \nData do evento \nLocal do evento \nOrçamento \nEscolha: ")
+
+    arquivo = open(arquivo_nome, "r")
+    for linha in arquivo:
+            dados_novos.append(linha.strip())
+    arquivo.close()
+                               
+    if opcao == "nome":
+        novo_nome = input("Digite o novo nome: ")
+        dados_novos[0] = novo_nome
+        nome_evento_arquivo = novo_nome.replace(' ', '_')
+        arquivo_nome_novo = f"{nome_evento_arquivo}.txt"
+        os.remove(arquivo_nome)
+
+        with open(arquivo_nome_novo, "w") as arquivo:
+            for itens in dados_novos:    
+                arquivo.write(itens + '\n')
+        print(f"Arquivo com novo nome {arquivo_nome_novo}")
+        print(f"Para acessar use: {novo_nome}")
+                 
+
+    elif opcao == "tipo":
+        novo_tipo = input("Digite o novo tipo: ")
+        dados_novos[1] = novo_tipo
+
+    elif opcao == "data":
+        nova_data = input("Digite a nova data desta forma (XX/YY/ZZZZ): ")
+        dados_novos[2] = nova_data
+            
+    elif opcao == "local":
+        novo_local = input("Digite o novo local: ")
+        dados_novos[3] = novo_local
+
+    elif opcao == "orc":
+        nova_orc = input("Digite o novo orçamento: ")
+        dados_novos[4] = nova_orc
+
+    if opcao != "nome":
+        for itens in dados_novos:    
+            arquivo.write(itens + '\n')
+
+def calcular_lucro_bruto(nome_evento):
+    buscar_orcamento = []
+    nome_evento_arquivo = nome_evento.replace(' ', '_')
+    arquivo_nome = f"{nome_evento_arquivo}.txt"
+    with open(arquivo_nome, "r", encoding="utf-8") as arquivo:
+        for linha in arquivo:
+            buscar_orcamento.append(linha.strip())
+    orcamento_do_evento = float(buscar_orcamento[4])
+
+    while True:
+        try:
+            opcao = int(input("Opção[1] Adicionar os custos um a um\nOpção[2] Já tenho os custos somados\nOpção[3] Sair\n"))
+
+            if opcao == 1:
+                custos_do_evento = 0
+                while True:
+                    custo = input("\nPara ver o resultado digite [sair]\nInsira o número usando apenas digitos:  ")
+                    os.system('cls')
+                    print(f"O valor de {custo} foi inserido com sucesso!!")
+
+                    if custo == "sair":
+                        lucro_bruto = orcamento_do_evento - custos_do_evento
+                        print(f"O lucro bruto do evento {nome_evento} é de R${lucro_bruto}\n")
+
+                    elif custo != "sair":
+                        custo = float(custo)
+                        custos_do_evento = custos_do_evento + custo
+
+            elif opcao == 2:
+                custo_somado = float(input("\nPara ver o resultado digite [sair]\nInsira o custo somado do evento utilizando digitos:  "))
+                os.system('cls')
+                lucro_bruto = orcamento_do_evento - custo_somado
+                print(f"O lucro bruto do evento {nome_evento} é de R${lucro_bruto}\n")
+                break
+            elif opcao == 3:
+                print("Voltando ao menu. ")
+                os.system('cls')
+                break
+        except ValueError:
+            print("Escolha a opção usando digitos 1 ou 2.")
+            
+
+def retornando_data_str_E_in_var():
+    # essa função retornar a data de hoje (na ordem certa e sem o tempo exato que a data foi requerida ) e o dia 
+    # o mes e o ano armazenado como inteiros dentro de variáveis 
+    # teste essa função usando a biblioteca datetime e pedindo para printar 
+    #dia_hj , mes_hj , ano_hj
+    date_today_gr = (datetime.now())
+    # vai sair no formato gringo 2025-11-06
+    # precisa-se transformar no formato brasileiro em formato brasileiro 06-11-2025
+    date_today_gr = str(date_today_gr)
+    # por isso vai ser feito um macete para transformar o formato gringo em br
+    # 1.pegando o date today e fazendo dele uma lista para
+    # separarmos cada parte da data pelo separador '-'
+    date_today_list = date_today_gr.split("-")
+
+    # obs : o último item da lista , correspondente ao dia , vem tambem com o
+    # os segundos , horas e minutos que essa data foi requerida , por isso vamos ter que
+    # capturar só os dois primeiros caracteres do último item da lista e colocalos
+    # no lugar da string com o tempo que a pessoa
+    #  requeriu essa data (tipo: "06 23:06:56.012503")
+    datetoday_Br = ""
+    #  =============isolando só o dia
+    item_dois_lista = list(date_today_list[2])
+    item_dois_lista = item_dois_lista[0:2]
+    item_dois_lista = "".join(item_dois_lista)
+    date_today_list[2] = item_dois_lista
+    # a seguir vai ser usado join , por que o date_today_list[0:2] , sem ele ,
+    # iria ser guardado na forma de lista
+    date_today_list[2] = "".join(date_today_list[2])
+    # ====================
+    datetoday_Br += date_today_list[2]
+    dia_hj = int(date_today_list[2])
+    # colocando a parte dia do mês
+    datetoday_Br += "-"
+    datetoday_Br += date_today_list[1]
+    mes_hj = int(date_today_list[1])
+    # colocando a parte do mês do ano
+    datetoday_Br += "-"
+    # colocando a parte do ano
+    datetoday_Br += date_today_list[0]
+    ano_hj = int(date_today_list[0])
+    return datetoday_Br, dia_hj, mes_hj, ano_hj
+
+
+date_hj, dia_hj, mes_hj, ano_hj = retornando_data_str_E_in_var()
--- main.py
+++ main.py
@@ -2,31 +2,40 @@ from funcoes import *
 import os
 os.system("cls")
 
+
 while True:
     print("--------------------MENU ORGANIZA FESTA--------------------")
-    opcoes = input(
-        "escolha o que você deseja fazer: \nAdicionar Eventos \nVisualizar Eventos \nEditar Eventos \nExcluir Eventos \nSair \nEscolha: ").lower()
+    opcoes = input("Escolha o que você deseja fazer: \nAdicionar  \nVisualizar \nEditar \nLucro Bruto  \nExcluir \nTarefa e Orcamento \nSair \nEscolha: ").lower()
 
     if opcoes == "sair":
         break
 
     elif opcoes == "adicionar" or opcoes == "add":
-        nome_do_evento = input("Insira o noem do evento: ").capitalize().strip()
+        nome_do_evento = input("Insira o nome do evento: ").capitalize().strip()
         tipo_do_evento = input("Insira o tipo de evento: ").capitalize().strip()
         data_do_evento = input("Insira a data desta forma (XX/YY/ZZZZ): ")
         local_do_evento = input("Insira o local do evento: ").capitalize().strip()
         orcamento_do_evento = input("Insira o orçamento utilizando digitos: ")
         adicionar(nome_do_evento, tipo_do_evento, data_do_evento, local_do_evento, orcamento_do_evento)
-        continue
-
+        
     elif opcoes == "visualizar" or opcoes == "visu":
+        # captura a data de hoje 
+        # pegando a data no formato gringo , transforma no brasileiro e atribui à uma variável o dia do mês , o mês e o ano em inteiro usando a função abaixo
+        data_hj , dia_hj , mes_hj , ano_hj = retornando_data_str_E_in_var()
         nome_do_evento = input("Insira o nome do evento: ").capitalize().strip()
         print()
         visualizar(nome_do_evento)
-        continue
-
+        
     elif opcoes == "excluir" or opcoes == "exc":
-        continue
+        nome_do_evento = input("Insira o nome do evento que deseja apagar: ").capitalize().strip()
+        excluir(nome_do_evento)
 
-    elif opcoes == "editar" or opcoes == "edt":
-        continue
+    elif opcoes == "lucro bruto" or "lb":
+        nome_do_evento = input("Insira o nome do evento que deseja editar: ").capitalize().strip()
+        calcular_lucro_bruto(nome_do_evento)
+
+
+    elif opcoes == "editar" or opcoes == "edit":
+        nome_do_evento = input("Insira o nome do evento que deseja editar: ").capitalize().strip()
+        editar(nome_do_evento)
+        
