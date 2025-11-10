9f207318bb089713ab1e9bb747adf507ef0fd435
Gabrielorrico <agbo@cesar.school>
2025-11-10 19:36:05 -0300

    Deixei as interações entre as funçoes em um padrao e adicionei uma função de convidados

--- main.py
+++ main.py
@@ -4,45 +4,99 @@ os.system("cls")
 
 
 while True:
-    print("--------------------MENU ORGANIZA FESTA--------------------")
-    opcoes = input("Escolha o que você deseja fazer: \nAdicionar  \nVisualizar \nEditar \nLucro Bruto  \nExcluir \nTempo \nTarefa e Orcamento \nSair \nEscolha: ").lower()
+    print("------------------------------------------------------------")
+    print("                 GERENCIADOR DE EVENTOS                    ")
+    print("------------------------------------------------------------")
+    print("1 - Adicionar evento")
+    print("2 - Visualizar evento")
+    print("3 - Editar evento")
+    print("4 - Excluir evento")
+    print("5 - Ver tempo restante")
+    print("6 - Tarefas e orçamento")
+    print("7 - Cadastrar fornecedor")
+    print("8 - Sugestão de evento")
+    print("9 - Lista de convidados do evento")
+    print("10 - Sair")
+    print("------------------------------------------------------------")
 
-    if opcoes == "sair":
+    opcoes = input("Escolha a opção desejada: ").lower()
+    
+    if opcoes == "sair"  or opcoes == "10":
         break
 
-    elif opcoes == "adicionar" or opcoes == "add":
+    elif opcoes == "adicionar" or opcoes == "add" or opcoes == "1":
+        print("\n" + "-" * 60)
+        print("  CADASTRAR NOVO EVENTO".center(60))
+        print(("-" * 60) + "\n")
+
         nome_do_evento = input("Insira o nome do evento: ").capitalize().strip()
         tipo_do_evento = input("Insira o tipo de evento: ").capitalize().strip()
         data_do_evento = input("Insira a data desta forma (XX/YY/ZZZZ): ")
         local_do_evento = input("Insira o local do evento: ").capitalize().strip()
         adicionar(nome_do_evento, tipo_do_evento, data_do_evento, local_do_evento)
         orcamento_do_evento = tarefas_orcamento(nome_do_evento)
+        print("\n Evento cadastrado com sucesso!")
         
-    elif opcoes == "visualizar" or opcoes == "visu":
+    elif opcoes == "visualizar" or opcoes == "visu" or opcoes == "2":
+        print("\n" + "-" * 60)
+        print("  VISUALIZAR EVENTOS".center(60))
+        print(("-" * 60) + "\n")
+
         nome_do_evento = input("Insira o nome do evento: ").capitalize().strip()
         print()
         visualizar(nome_do_evento)
         
-    elif opcoes == "excluir" or opcoes == "exc":
+    elif opcoes == "excluir" or opcoes == "exc" or opcoes == "3":
+        print("\n" + "-" * 60)
+        print("  EXCLUIR UM EVENTO".center(60))
+        print(("-" * 60) + "\n")
+
         nome_do_evento = input("Insira o nome do evento que deseja apagar: ").capitalize().strip()
         excluir(nome_do_evento)
 
-    elif opcoes == "editar" or opcoes == "edit":
+    elif opcoes == "editar" or opcoes == "edit" or opcoes == "4":
+        print("\n" + "-" * 60)
+        print("  EXCLUIR UM EVENTO".center(60))
+        print(("-" * 60) + "\n")
+
         nome_do_evento = input("Insira o nome do evento que deseja editar: ").capitalize().strip()
         editar(nome_do_evento)
 
-    elif opcoes == "tempo" or opcoes == "time":
+    elif opcoes == "tempo" or opcoes == "time" or opcoes == "5":
+        print("\n" + "-" * 60)
+        print("  VISUALIZAR QUANTO TEMPO FALTA PARA UM EVENTO".center(60))
+        print(("-" * 60) + "\n")
         nome_do_evento = input("Insira o nome do evento que deseja ver quanto tempo falta: ").capitalize().strip()
         tempo_restante_evento(nome_do_evento)
 
-    elif opcoes == "tarefa e orcamento" or opcoes == "to":
+    elif opcoes == "tarefa e orcamento" or opcoes == "to" or opcoes == "6":
+        print("\n" + "-" * 60)
+        print("  CADASTRAR UM ORÇAMENTO".center(60))
+        print(("-" * 60) + "\n")
+
         nome_do_evento = input("Insira o nome do evento que deseja adcionar as tarefas e atualizar o orçamento: ").capitalize().strip()
         tarefas_orcamento(nome_do_evento)
 
-    elif opcoes == "cadastrar" or opcoes == "forn":
+    elif opcoes == "cadastrar" or opcoes == "forn" or opcoes == "7":
+        print("\n" + "-" * 60)
+        print("  CADASTRAR FORNECEDORES".center(60))
+        print(("-" * 60) + "\n")
+
         cadastrar_fornecedores()
 
-    elif opcoes == "sugestao" or opcoes == "sug":
+    elif opcoes == "sugestao" or opcoes == "sug" or opcoes == "8":
+        print("\n" + "-" * 60)
+        print("  SUGESTÕES DE EVENTOS".center(60))
+        print(("-" * 60) + "\n")
+
         nome_do_evento = input("Insira o nome do evento que deseja ter uma sugestao ").capitalize().strip()
         oferecer_sugestoes(nome_do_evento)
+
+    elif opcoes == "convidados" or opcoes == "9":
+        print("\n" + "-" * 60)
+        print("  LISTA DE CONVIDADOS".center(60))
+        print(("-" * 60) + "\n")
+
+        nome_do_evento = input("Insira o nome do evento que deseja ver a lista de convidados: ").capitalize().strip()
+        convidados_evento(nome_do_evento)
         
